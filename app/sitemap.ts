d381b2c50d8089fc026f9a1f505c1786bdd0944c
Lyzen Vira <lyzen20vm@gmail.com>
2024-12-03 17:48:36 +0200

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -2,15 +2,15 @@ import { Product } from "@/config/types";
 import { getProducts } from "@/services/ProductService";
 
 export default async function sitemap() {
-  const data = await getProducts();
-  let getTitle =[{}];
+//   const data = await getProducts();
+//   let getTitle =[{}];
 
-  if(data){
-  getTitle = data.products.map((product: Product) => ({
-    url: `https://timestone.com/catalog/${product.handle}`,
-    lastModified: new Date(),
-  }));
-}
+//   if(data){
+//   getTitle = data.products.map((product: Product) => ({
+//     url: `https://timestone.com/catalog/${product.handle}`,
+//     lastModified: new Date(),
+//   }));
+// }
   return [
     { url: "https://timestone.com", lastModified: new Date() },
     { url: "https://timestone.com/catalog", lastModified: new Date() },
@@ -18,6 +18,6 @@ export default async function sitemap() {
     { url: "https://timestone.com/contact-us", lastModified: new Date() },
     { url: "https://timestone.com/auth", lastModified: new Date() },
     { url: "https://timestone.com/", lastModified: new Date() },
-    ...getTitle,
+   //  ...getTitle,
   ];
 }
